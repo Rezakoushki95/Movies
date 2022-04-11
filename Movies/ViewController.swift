72a6937ff81d70d5c8aeba3d1db1e0fd7f99c009
rezakoushki95 <rezaaarhus@hotmail.com>
2022-04-11 13:36:04 +0200

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Reza Koushki on 07/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	@IBOutlet weak var tableView: UITableView!
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
